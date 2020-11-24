38b0ed795a647dc27d1b6966a2d8cf7dc0f9eaf4
Matias Mendez <matiasm@avalith.net>
2020-11-24 18:30:09 -0300

    second-day

--- src/main/java/Controller/UserController.java
+++ src/main/java/Controller/UserController.java
@@ -5,7 +5,10 @@ import Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
@@ -13,11 +16,16 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @RequestMapping("/index")
+    public String startingPoint(Model model){
+        return "test";
+    }
+
     @PostMapping("/login")
-    public String userLogin (Login login, Model model){
+    public String userLogin (@ModelAttribute Login login, Model model){
         if(userService.validateUser(login))
             return "main";
         else
-            return "wrongUser";
+            return "wrong-user";
     }
 }
